eff70618540f1cb2ea6ea401b241f11b9c37b25a
Andrew Kiellor <akiellor@gmail.com>
2014-11-17 09:41:56 -0800

    Added symlinks for apm and atom launcher.

--- modules/atom/manifests/init.pp
+++ modules/atom/manifests/init.pp
@@ -1,8 +1,22 @@
 class atom {
-  package { 'atom':
+  package { 'Atom':
     ensure   => installed,
     source   => 'https://atom.io/download/mac',
     provider => compressed_app,
     flavor   => 'zip',
+  }
+
+  file { "${boxen::config::bindir}/apm":
+    ensure  => link,
+    target  => '/Applications/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm',
+    mode    => '0755',
+    require => Package['Atom']
+  }
+
+  file { "${boxen::config::bindir}/atom":
+    ensure  => link,
+    target  => '/Applications/Atom.app/Contents/Resources/app/atom.sh',
+    mode    => '0755',
+    require => Package['Atom']
   }
 }
